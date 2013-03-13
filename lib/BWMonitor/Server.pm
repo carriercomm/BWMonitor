ea2f35c22202fb9dfd374e41428a9adc8750370d
Odd Eivind Ebbesen <odd@oddware.net>
2013-03-13 19:03:33 +0100

    Added BWMonitor::Server.pm

---
+++ lib/BWMonitor/Server.pm
@@ -0,0 +1,18 @@
+# Licence: GPL
+# Author: Odd Eivind Ebbesen <odd@oddware.net>
+# Date: 2013-03-13 19:02:07
+
+package BWMonitor::Server;
+
+use strict;
+use warnings;
+
+sub new {
+}
+
+sub run {
+}
+
+1;
+__END__
+
